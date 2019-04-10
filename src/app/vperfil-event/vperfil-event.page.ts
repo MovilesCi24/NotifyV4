0718e09fb9af278cd91ee7b60cb852f6c23dba2e
MovilesCi24 <49313550+MovilesCi24@users.noreply.github.com>
2019-04-10 15:17:03 -0500

    ASA

--- src/app/vperfil-event/vperfil-event.page.ts
+++ src/app/vperfil-event/vperfil-event.page.ts
@@ -15,6 +15,14 @@ export class VperfilEventPage implements OnInit {
   Data;
   Id_Perfil;
   Datos;
+  Devices;
+  Dispositivo=new Array();
+  Componentes=new Array();
+  Eventos=new Array();
+  TDisp=new Array();
+  Tcomp=new Array();
+  Vevent=new Array();
+  Enviado=new Array();
   constructor(
     public global:GlobalService,
     public Post:PostService,
@@ -31,6 +39,18 @@ export class VperfilEventPage implements OnInit {
   }
 
   ngOnInit() {
+    let data1={
+      Option:"GetDevices"
+    };
+    this.Post.Event(data1,(err,data)=>{
+      console.log(data) ;
+      if(err==null){
+        this.Devices=JSON.parse(data.data);
+        this.OrganizarData(this.Devices);
+      }else{
+        this.Alert.AlertOnebutton('Error',JSON.stringify(err.message));
+      }
+  });
     let data={
       Option:"EventProfile",
       Id_ProfileEvent:this.Id_Perfil
@@ -39,14 +59,97 @@ export class VperfilEventPage implements OnInit {
       console.log(data) ;
       if(err==null){
         this.Datos=JSON.parse(data.data);
-        this.OrganizarData(this.Datos);
       }else{
         this.Alert.AlertOnebutton('Error',JSON.stringify(err.message));
       }
   });
   }
 
-  OrganizarData(data){
-
+  OrganizarData(Disp){
+    let map = new Map();
+    for (let item of Disp) {
+        if(!map.has(item.Id_DeviceType)){
+            map.set(item.Id_DeviceType, true);    // set any value to Map
+            this.Dispositivo.push({
+              Id_DeviceType: item.Id_DeviceType,
+              NameDevices: item.NameDevices
+            });
+            this.TDisp.push(false);
+        }
+    }
+    this.Componentes=new Array(this.Dispositivo.length);
+    this.Tcomp=new Array(this.Dispositivo.length);
+    for(let i=0;i<this.Dispositivo.length;i++){
+      this.Componentes[i]=new Array();
+      this.Tcomp[i]=new Array();
+      let map2 = new Map();
+      for (let item of Disp) {
+        if((!map2.has(item.Id_Component))&&(item.Id_DeviceType==this.Dispositivo[i].Id_DeviceType)){
+            map2.set(item.Id_Component, true);    // set any value to Map
+            this.Componentes[i].push({
+              Id_Component: item.Id_Component,
+              NameComponent: item.NameComponent
+            });
+            this.Tcomp[i].push(false);
+        }
+    }
+    }
+    this.Eventos=new Array(this.Dispositivo.length);
+    this.Vevent=new Array(this.Dispositivo.length);
+    for(let i=0;i<this.Dispositivo.length;i++){
+      this.Eventos[i]=new Array(this.Componentes[i].length);
+      this.Vevent[i]=new Array(this.Componentes[i].length);
+      for(let j=0;j<this.Componentes[i].length;j++){
+        this.Eventos[i][j]=new Array();
+        this.Vevent[i][j]=new Array();
+        let map3 = new Map();
+        for (let item of Disp) {
+          if((!map3.has(item.Id_EventComponent))&&(item.Id_DeviceType==this.Dispositivo[i].Id_DeviceType)&&(item.Id_Component==this.Componentes[i][j].Id_Component)){
+              map3.set(item.Id_EventComponent, true);    // set any value to Map
+              this.Eventos[i][j].push({
+                Id_DeviceType: item.Id_DeviceType,
+                NameDevices: item.NameDevices,
+                Id_Component: item.Id_Component,
+                NameComponent: item.NameComponent,
+                Id_EventComponent: item.Id_EventComponent,
+                NameEvent: item.NameEvent,
+                Id_DeviceComponent: item.Id_DeviceComponent
+              });
+              this.Vevent[i][j].push(false);
+          }
+      }
+    }
   }
+  console.log(this.Eventos);
+}
+
+VerificarExis(){
+  for(let o=0;o<this.Datos.length;o++){
+    for(let i=0;i<this.Dispositivo.length;i++){
+      for(let j=0;j<this.Componentes[i].length;j++){
+        for(let k=0;k<this.Eventos[i][j].length;k++){
+          if(this.Datos[o].Id_EventComponent==this.Eventos[i][j][k].Id_EventComponent){
+            this.Vevent[i][j][k]=true;
+          }
+        }
+      }
+    }
+  }
+}
+
+ToggleD(i){
+  if(this.TDisp[i]==false){
+    this.TDisp[i]=true;
+  }else if(this.TDisp[i]==true){
+    this.TDisp[i]=false;
+  }
+}
+
+ToggleC(i,j){
+  if(this.Tcomp[i][j]==false){
+    this.Tcomp[i][j]=true;
+  }else if(this.Tcomp[i][j]==true){
+    this.Tcomp[i][j]=false;
+  }
+}
 }
